1b955993c8067ea846b1e5f6049106a52e3d8e4f
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-09-12 12:25:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/node_bc_52_ann1777">
+        <BrowserRouter basename="/react_node_bc_52_ann1777/">
           <App />
         </BrowserRouter>
       </PersistGate>
